86cf97f8c3bea44b6168359829aa986d957e3acb
pamattei <pierrealex.mattei@gmail.com>
2017-10-02 18:54:17 +0200

    revised demo

--- demoGSPPCA.R
+++ demoGSPPCA.R
@@ -1,22 +1,25 @@
-set.seed(123)
+
+source("GSPPCAv1.R")
+
+set.seed(1234)
 
 # First, we simulate some data according to a simple globally sparse PPCA model
 
-n = 100
+n = 50
 p = 500
 d = 10 # dimension of the latent space
-q = 50 # true number of relevant variables
+q = 20 # true number of relevant variables
 
 vtrue = c(rep(0,p-q),rep(1,q)) # true sparsity pattern
 sigma = 1
-W = diag(vtrue) %*% matrix(rnorm(p*d),nrow=p)
+W = diag(vtrue) %*% (matrix(rnorm(p*d),nrow=p))
 X = matrix(rnorm(n*d),nrow=n) %*% t(W) + sigma*matrix(rnorm(p*n),nrow=n)
 
 
 # Now we can run the GSPPCA algorithm
 # On this 100x500 example, it takes less than 10 seconds on a MacBook Pro i5 processor
 
-out = gsppca(X,d=10,epsi=1e-4,method="JL",verbose=TRUE)
+out = gsppca(X,d=10,epsi=1e-4,method="BC",verbose=TRUE)
 
 # and check if the true sparsity pattern is recovered
 
@@ -34,3 +37,4 @@ vhatspca = spca(X,K = 1,sparse = "varnum",para = q)$loadings!=0
 # We can see that even if we choose the true number of relevant variables, SPCA makes many mistakes
 
 print(sum(abs(vtrue-vhatspca)))
+
