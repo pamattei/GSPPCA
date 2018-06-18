fb5be55f887b3d4d07704f3eadab18859748ab04
pamattei <pierrealex.mattei@gmail.com>
2018-06-18 11:45:23 +0200

    Update GSPPCA v3

--- GSPPCAv1.R
+++ GSPPCAv1.R
@@ -4,23 +4,21 @@
 
 # References:
 # [1] P.-A. Mattei, C. Bouveyron and P. Latouche, Globally Sparse Probabilistic PCA, Proc. AISTATS 2016, pp. 976-984
-# [2] C. Bouveyron, P. Latouche and P.-A. Mattei, Bayesian Variable Selection for Globally Sparse Probabilistic PCA, HAL preprint 01310409
+# [2] C. Bouveyron, P. Latouche and P.-A. Mattei, Bayesian Variable Selection for Globally Sparse Probabilistic PCA, Electronic Journal of Statistics, in press
 
 # IMPORTANT REMARK: we use the model described in [2] rather than [1]. These models simply differ by the parametrization of alpha.
 
 # Contact: pima@itu.dk
 # http://pamattei.github.io
 
-tr=function(A) sum(diag(A))
-trprod=function(A,B) sum(A*t(B)) # efficiently computes tr(A %*% B)
-logdet=function(A) determinant(A,logarithm = TRUE)$modulus[1]
+
 
 # This function implements the VEM algorithm described in section 2 of [2]
 #
 # INPUT:
 # data matrix x in R^{n*p} (each row is one observation)
 # dimension of the latent space d in N*
-# number of iterations nit in N*
+# maximum number of iterations nit in N*
 # convergence criterion epsi>0 (the VEM stops when the relative difference of free energy becomes lower than epsi)
 # initialization value alpha>0 (smaller alpha might lead to sparser solutions)
 # initialization value s>0
@@ -45,6 +43,7 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
   ## Hyperparameter initialisation
   if (is.null(u)) u = rep(1,p)
   if (is.null(alpha)) alpha = 1
+  #if (is.null(u)) u = rbeta(p, shape1=9, shape2=1) # use this for random initialization of alpha
   
   ## A posteriori parameters initialisation
   if (is.null(Sigma)) Sigma = diag(d)
@@ -59,7 +58,7 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
   j = 0
   reldiff = epsi+10
   
-  while ((j<nit)&(reldiff>epsi)) {
+  while ((j<nit)&((reldiff>epsi))) {
     
     j=j+1
     
@@ -80,14 +79,15 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
     ## M-Step
     s2 = (n*p)^(-1)*(trcrossprodx+trprod(n*Sigma+crossprodmu,crossprod(u*M)+apply(Su,c(1,2),sum))-2*trprod(x%*%(u * M), t(mu)))
     if (d==1) s2 = (n*p)^(-1)*(trcrossprodx+trprod(n*Sigma+crossprodmu,crossprod(u*M)+apply(Su,c(1,2),sum))-2*trprod(x%*%(u * M), t(mu)))
-   
-     s = sqrt(s2)
+    
+    s = sqrt(s2)
     alpha = ((trprod(t(M),M) +tr(apply(S,c(1,2),sum)))/(d*p))^(-1/2)
+    uold=u
     u = sapply(1:p,function(k){
-      trprod(S[,,k]+tcrossprod(M[k,]),n*Sigma+crossprodmu)^(-1)*crossprod(M[k,],t(mu)%*%x[,k]*s2)
+      crossprod(M[k,],t(mu)%*%x[,k])/trprod(S[,,k]+tcrossprod(M[k,]),n*Sigma+crossprodmu)
     })
-    id0 = which(u<0)
-    if(length(id0)>0) u[id0] = .Machine$double.eps
+    id0 = which(u<=10^(-200))
+    if(length(id0)>0) u[id0] = 10^(-200)
     id1 = which(u>1)
     if(length(id1)>0) u[id1] = 1
     
@@ -98,6 +98,7 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
     if (verbose==TRUE) {cat(j); cat(' ')}
     if (j>10) reldiff=abs((L[j]-L[j-1])/L[j])
   }
+  
   return(list(u=u,L=L[1:j],reldiff=reldiff,alpha=alpha,s=s,Sigma=Sigma,mu=mu,S=S,M=M))
 }
 
@@ -106,12 +107,14 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
 #
 # INPUT:
 # data matrix x in R^{n*p} (each row is one observation)
-# vector of variables weights u in [0,1]^p (in the case of the GSPPCA algorithm, u is the first output of the VEM, but other orderings can be tested)
+# vector of variables weights u in [0,1]^p (in the case of the GSPPCA algorithm, u is the first output of the VEM, but other orderings can be used)
 # dimension of the latent space d in N*
 # pmin and pmax are the minimum and maximum cardinalities of tested models
-# method is the method used for noise variance estimation, can either be "JL" (Johnstone & Lu), "BC" (bias-corrected) or "ML" (maximum-likelihood)
-# alpha0 corresponds to the initialization value for gradient ascent, alpha0="auto" chooses automatically with the method of moments
-# svdobject allows to pass the results of a previously done svd                             
+# method is the method used for noise variance estimation, can either be "JL" (Johnstone & Lu), "BC" (bias corrected) or "ML" (maximum likelihood)
+# svdobject allows to pass to the algorithm the results of an alreadly performed SVD
+# prior corresponds to the prior on the sparsity pattern, it can be either "uniform" or "info"
+# the informative prior is sparsity-inducing: it assumes that the prior probability of selecting more than n variables is at most levelinfprior (whose default is 5%)
+
 #
 # OUTPUT:
 # qhat in [(d+1),p], number of relevant variables selected by the algorithm
@@ -119,7 +122,7 @@ VEM<-function(x,d,nit=100,epsi=0.0001,alpha=1,s=1,scale=FALSE,verbose=FALSE,u=NU
 
 
 
-Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",alpha0="auto",svdobject=NULL){
+Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",svdobject=NULL,prior="uniform",levelinfprior=0.05){
   
   ## Reorganize the variables according to the ordering of the coefficients of u
   xord <- scale(x[,order(u,decreasing=T)],scale=FALSE)
@@ -133,7 +136,7 @@ Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",alpha0="auto",svdobj
   
   ## Discard the variables whose u coefficient is exactly zero (ARD effect)
   
-  pmax = min(pmax,p-sum(u<.Machine$double.eps))
+  pmax = min(pmax,p-sum(u<=10^(-200)))
   
   dims=seq(pmin,pmax,1) # cardinalities of the models tested
   
@@ -157,18 +160,19 @@ Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",alpha0="auto",svdobj
       return(-Dlogevi)
     }
     
-    if (is.numeric(alpha0)==TRUE) opt = optim(alpha0,fn=neglogl,gr=Dneglogl,method ="L-BFGS-B",lower = 10^(-10), upper = Inf)
-    else opt = optim(sqrt(d*n*k/sum(R^2)),fn=neglogl,gr=Dneglogl,method ="L-BFGS-B",lower = 10^(-10), upper = Inf)
-    return(list(logmarginal=-opt$value,alpha=opt$par,count=opt$count[1])) }
+    opt = optim(sqrt(d*n*k/sum(R^2)),fn=neglogl,gr=Dneglogl,method ="L-BFGS-B",lower = 10^(-10), upper = Inf)
+    return(list(logmarginal=-opt$value)) }
   
   ## Compute the noiseless marginal likelihood
   
   resu = sapply(dims,function(k)computelogmarginal(k))
-  l=unlist(unlist(resu[1,]))
-  alphass=unlist(unlist(resu[2,]))
-  counts=unlist(unlist(resu[3,]))
+  if (prior == "uniform") l=unlist(unlist(resu))
+  if (prior == "info") {
+    rho=infprior(pmax,n,alpha = levelinfprior)
+    l= unlist(unlist(resu)) + (log(rho)-log(1-rho))*dims
+  }
   
-  return(list(qhat=dims[which.max(l)],l=l))
+  return(list(qhat=dims[which.max(l)],l=l,s=s))
 }
 
 # This function implements the whole GSPPCA algorithm
@@ -176,7 +180,7 @@ Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",alpha0="auto",svdobj
 # INPUT:
 # data matrix x
 # dimension of the latent space d
-# some other parameters from the VEM and Occam functions 
+# some other parameters from the VEM and Occam functions (in particular, we can choose several starting points for the alpha parameter, each one is used to run 3 iterations and the best one is chosen)
 # numvar="Occam" indicates that you use the noiseless marginal for model selection
 # but you can also use an integer argument if you already know the number of relevant variables
 #
@@ -188,9 +192,16 @@ Occam <- function(x,u,d,pmin=d+1,pmax=ncol(x)-1,method="BC",alpha0="auto",svdobj
 # center is the centering used before performing PCA
 
 
-gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FALSE,numvar="Occam",pmin=(d+1),pmax=ncol(x)-1,method="BC",svdfree=FALSE,verbose=FALSE,alpha0="MoM"){
+gsppca <- function (x,d="autoGCV",nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FALSE,numvar="Occam",prior="uniform",levelinfprior=0.05,pmin=(d+1),pmax=ncol(x)-1,method="BC",svdfree=FALSE,verbose=FALSE){
   
   n <- nrow(x); p <- ncol(x)
+  
+  if (d=="autoGCV") {
+    d=FactoMineR::estim_ncp(x,1,floor(p/2),scale=scale,method = "Smooth")$ncp
+    # pmax=floor((d+p)/2)
+  }
+  
+  
   
   ## Preprocessing (scaling + SVD)
   
@@ -220,6 +231,8 @@ gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FA
   
   ### VEM algorithm  
   
+  if (verbose==TRUE) {cat('VEM iterations: ')}
+  
   if (svdfree==FALSE) {
     if (scale==TRUE) {
       if (alphainit=="auto") alphainit=sqrt(d/(1)) # computed with the method of moments
@@ -240,21 +253,22 @@ gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FA
       outVEM <- VEM(x=xc,d=d,nit=nit,alpha=alphainit,s=shat,mu=matrix(rnorm(d*n),ncol=d),M=matrix(rnorm(d*p),ncol=d),epsi=epsi,scale=FALSE,verbose=verbose)
     }
   }
- 
+  
   u <- outVEM$u
   
-  if (sum(u<.Machine$double.eps)>=(p-d)) stop("VEM converged to u=0 !")
+  if (sum(u<=10^(-200))>=(p-d)) stop("VEM converged to u=0 !")
   
   # Model selection
   if (numvar=="Occam") { if (verbose==TRUE) {cat('\n'); cat('Model Selection...')}
-    if (alpha0=="VEM") res <- Occam(x=xc,u=u,d=d,pmin=pmin,pmax=pmax,method=method,alpha0=out$alpha,svdobject = dvs)
-    else res <- Occam(x=x,u=u,d=d,pmin=pmin,pmax=pmax,method=method,alpha0='auto',svdobject = dvs)
+    res <- Occam(x=x,u=u,d=d,pmin=pmin,pmax=pmax,method=method,svdobject = dvs,prior=prior,levelinfprior=levelinfprior)
     if (verbose==TRUE) cat('done.')
     qhat<-res$qhat
     l<-res$l}
   else qhat<-numvar
   vhat = rep(0,p)
   vhat[order(u,decreasing=TRUE)[1:qhat]] = 1
+  if (numvar=="Occam") {alpha=res$alpha
+  s=res$s}
   
   # PCA on the selected variables
   outpca = prcomp(x[,vhat==1],retx = )
@@ -263,8 +277,8 @@ gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FA
   center = colMeans(x)
   
   if (numvar=="Occam") {
-    return(list(l=l,vhat=vhat,qhat=qhat,u=u,loadings=loadings,scores=outpca$x[,1:d],center=center,counts=res$counts))}
-  else return(list(vhat=vhat,qhat=qhat,u=u,loadings=loadings,scores=outpca$x[,1:d],center=center))
+    return(list(l=l,vhat=vhat,d=d,qhat=qhat,u=u,loadings=loadings,scores=outpca$x[,1:d],center=center,alpha=alpha,s=s))}
+  else return(list(vhat=vhat,d=d,qhat=qhat,u=u,loadings=loadings,scores=outpca$x[,1:d],center=center))
 }
 
 
@@ -274,7 +288,7 @@ gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FA
 #
 # REFERENCES :
 #
-# [1] D. Passemier, Z. Li, J. Yao, On estimation of the noise variance in high-dimensional probabilistic principal component analysis, JRSSB 2016
+# [1] D. Passemier, Z. Li, J. Yao, On estimation of the noise variance in high-dimensional probabilistic principal component analysis, JRSSB 2017
 # [2] M. Tipping and C. Bishop, Probabilistic principal component analysis, JRSSB 1999
 #
 # INPUT:
@@ -285,38 +299,57 @@ gsppca <- function (x,d,nit=200,alphainit="auto",sinit="auto",epsi=1e-5,scale=FA
 # OUTPUT:
 # noise standard error estimator s
 #
-# Contact: pierrealexandre.mattei@parisdescartes.fr
+# Contact: pima@itu.dk
 # http://pamattei.github.io/
 
 
 sdPPCA <- function(x,d,method="BC",svdobject=NULL){
   
   if (method!="JL"){
-  n <- as.numeric(nrow(x))
-  p <- as.numeric(ncol(x))
-  
-  ratio <- as.numeric(p/n)
-  
-  if (is.null(svdobject)==1) svdobject=svd(x,nu=0,nv=0)
-  
-  E <- c(rep(0,p-min(n,p)),sort(svdobject$d^2)/n)
-  sML2 <- sum(E[1:(p-d)])/(p-d)
-  if (method=="BC"){
-    alpha=numeric(d)
-    for (j in 1:d){
-      alpha[j] = 0.5*(sqrtplus((sML2*(ratio+1)-E[p-j+1])^2-4*ratio*sML2^2)-sML2*(ratio+1)+E[p-j+1])}
-    b=sqrt(ratio/2)*(d+sML2*sum(1/alpha))
+    n <- as.numeric(nrow(x))
+    p <- as.numeric(ncol(x))
     
-    sPY2=sML2+(b*sML2*sqrt(2*ratio))/(p-d)}
-  
-  if (method=="BC") s=sqrt(sPY2)
-  if (method=="ML") s=sqrt(sML2)}
+    ratio <- as.numeric(p/n)
+    
+    if (is.null(svdobject)==1) svdobject=svd(x,nu=0,nv=0)
+    
+    E <- c(rep(0,p-min(n,p)),sort(svdobject$d^2)/n)
+    sML2 <- sum(E[1:(p-d)])/(p-d)
+    if (method=="BC"){
+      alpha=numeric(d)
+      for (j in 1:d){
+        alpha[j] = 0.5*(sqrtplus((sML2*(ratio+1)-E[p-j+1])^2-4*ratio*sML2^2)-sML2*(ratio+1)+E[p-j+1])}
+      b=sqrt(ratio/2)*(d+sML2*sum(1/alpha))
+      
+      sPY2=sML2+(b*sML2*sqrt(2*ratio))/(p-d)}
+    
+    if (method=="BC") s=sqrt(sPY2)
+    if (method=="ML") s=sqrt(sML2)}
   
   if (method=="JL")  { n <- as.numeric(nrow(x)); s=sqrt(median(apply(x,MARGIN = 2,FUN=function (x0) sum(x0^2)/(n-1)))) }
   
   return(s=s)
   
 }
+
+
+# This function computes an estimator of the projection matrix in a probabilistic PCA model
+#
+# REFERENCES :
+#
+# [1] D. Passemier, Z. Li, J. Yao, On estimation of the noise variance in high-dimensional probabilistic principal component analysis, JRSSB 2017
+# [2] M. Tipping and C. Bishop, Probabilistic principal component analysis, JRSSB 1999
+#
+# INPUT:
+# data matrix x in R^{n*p} (each row is one observation)
+# dimension of the latent space d in N*
+# method can be either "BC" which corresponds to the bias-corrected estimator of [1] or "ML" which corresponds to the classical ML estimator of [2]
+#
+# OUTPUT:
+# estimate of the projection matrix W
+#
+# Contact: pima@itu.dk
+# http://pamattei.github.io/
 
 MLPPCA <- function (X,d,method="BC",svdobject=NULL){
   if (is.null(svdobject)==1) svdobject=svd(X,nu=0,nv=d)
@@ -331,6 +364,26 @@ MLPPCA <- function (X,d,method="BC",svdobject=NULL){
   return(list(W=W,Y=t(Yt)))
 }
 
+
+# This function implements an informative (sparsity-inducing) prior for the model space {0,1}^p of GSPPCA
+
+
+
+
+infprior=function(p,n,alpha=0.05){
+  
+  fun=function(rho) pbinom(n,p,rho)-alpha
+  
+  if (fun(0.5)>0) rho = 0.5 else rho=uniroot(fun,c(1e-7,0.5))$root
+  
+  return(rho)
+}
+
+## A few useful functions
+
+tr=function(A) sum(diag(A))
+trprod=function(A,B) sum(A*t(B)) # efficiently computes tr(A %*% B)
+logdet=function(A) determinant(A,logarithm = TRUE)$modulus[1]
 
 sqrtplus=function(x)  sqrt((x>=0)*x)
 
